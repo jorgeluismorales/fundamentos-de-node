60a4b5be9d6f3695cdf9b12260d1209d04c832c9
jorgeluismorales <53586348+jorgeluismorales@users.noreply.github.com>
2021-07-20 20:18:38 -0300

    Se agregaron colores en la tabla en la salida por consola

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const argv = require("./config/yargs").argv;
+const colors = require("colors");
 const { crearArchivo } = require("./crearArchivo");
 console.log(argv);
 
 const base = argv.base;
 console.log(`base: ${base}`);
 
+console.log("==================".cyan);
+console.log(`tabla de ${base}`.red);
+console.log("==================".cyan);
+
 let resultado = "";
 
 for (j = 1; j < 11; j++) {
-  resultado += `${base} x ${j} = ${base * j}\n `;
+  resultado += `${colors.yellow(base)} ${"x".blue} ${colors.bgMagenta(j)} ${
+    "=".green
+  } ${base * j}\n `;
 }
+
+console.log(resultado);
 
 crearArchivo(resultado, base)
   .then((msj) => console.log(msj))
