d47cf727235a1645a11d66df8935d1faffb459ce
jorgeluismorales <53586348+jorgeluismorales@users.noreply.github.com>
2021-07-20 20:30:50 -0300

    Se arreglo problema con colors

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ console.log(`tabla de ${base}`.red);
 console.log("==================".cyan);
 
 let resultado = "";
+let resultadoConColor = "";
 
 for (j = 1; j < 11; j++) {
-  resultado += `${colors.yellow(base)} ${"x".blue} ${colors.bgMagenta(j)} ${
-    "=".green
-  } ${base * j}\n `;
+  resultadoConColor += `${colors.yellow(base)} ${"x".blue} ${colors.bgMagenta(
+    j
+  )} ${"=".green} ${base * j}\n `;
 }
 
-console.log(resultado);
+for (j = 1; j < 11; j++) {
+  resultado += `${base} x ${j} = ${base * j}\n `;
+}
+
+console.log(resultadoConColor);
 
 crearArchivo(resultado, base)
   .then((msj) => console.log(msj))
